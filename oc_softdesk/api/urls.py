1ce0897bf6192090437353c14f97888324698cdc
PabloLec <pablo.lecolinet@gmail.com>
2021-06-22 11:55:28 +0200

    Add viewsets and routers

---
+++ oc_softdesk/api/urls.py
@@ -0,0 +1,23 @@
+from django.urls import include, path
+from rest_framework_nested import routers
+from . import views
+
+projects_router = routers.SimpleRouter()
+projects_router.register(r"projects", views.ProjectViewSet)
+
+users_router = routers.NestedSimpleRouter(projects_router, r"projects", lookup="projects")
+users_router.register(r"users", views.ContributorViewSet, basename="users")
+
+issues_router = routers.NestedSimpleRouter(projects_router, r"projects", lookup="projects")
+issues_router.register(r"issues", views.IssueViewSet, basename="issues")
+
+comments_router = routers.NestedSimpleRouter(issues_router, r"issues", lookup="issues")
+comments_router.register(r"comments", views.CommentViewSet, basename="comments")
+
+urlpatterns = [
+    path("", include(projects_router.urls)),
+    path("", include(users_router.urls)),
+    path("", include(issues_router.urls)),
+    path("", include(comments_router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+]
--- oc_softdesk/api/views.py
+++ oc_softdesk/api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from .serializers import *
+from .models import *
 
-# Create your views here.
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ContributorViewSet(viewsets.ModelViewSet):
+    serializer_class = ContributorSerializer
+
+    def get_queryset(self):
+        return Contributor.objects.filter(project=self.kwargs["projects_pk"])
+
+
+class IssueViewSet(viewsets.ModelViewSet):
+    serializer_class = IssueSerializer
+
+    def get_queryset(self):
+        return Issue.objects.filter(project=self.kwargs["projects_pk"])
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.filter(issue=self.kwargs["issues_pk"])
